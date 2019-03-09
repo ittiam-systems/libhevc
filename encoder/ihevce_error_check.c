6147147cdf7dcd776b59107396d2d6e4a92c3850
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2019-03-08 18:18:46 -0800

    encoder: Allow minimum frame rate of 1
    Earlier min frame rate allowed was 7.5.
    Encoder now supports min frame rate of 1 which is needed for image
    encoding
    
    Bug: 132450110
    Test: With HeifWriter demo application for 512x512 image
    Change-Id: I1c39ecef3f489d9acc3ae1e6ef8f7ad587b3f2e1

--- encoder/ihevce_error_check.c
+++ encoder/ihevce_error_check.c
@@ -554,18 +554,18 @@ WORD32 ihevce_hle_validate_static_params(ihevce_static_cfg_params_t *ps_static_c
         return IHEVCE_SETUNSUPPORTEDINPUT(error_code);
     }
 
-    if(((ps_static_cfg_prms->s_src_prms.i4_frm_rate_num /
-         ps_static_cfg_prms->s_src_prms.i4_frm_rate_denom) > 120) ||
-       ((ps_static_cfg_prms->s_src_prms.i4_frm_rate_num /
-         ps_static_cfg_prms->s_src_prms.i4_frm_rate_denom) *
-            10 <
-        75))
+    if((((ps_static_cfg_prms->s_src_prms.i4_frm_rate_num * 1.0) /
+          ps_static_cfg_prms->s_src_prms.i4_frm_rate_denom) > MAX_FRAME_RATE) ||
+       (((ps_static_cfg_prms->s_src_prms.i4_frm_rate_num * 1.0) /
+          ps_static_cfg_prms->s_src_prms.i4_frm_rate_denom) < MIN_FRAME_RATE))
     {
         error_code = IHEVCE_FRAME_RATE_NOT_SUPPORTED;
         ps_sys_api->ihevce_printf(
             pv_cb_handle,
-            "IHEVCE ERROR:  Frame rate (i4_frame_rate_num & i4_frame_rate_denom) is out of range "
-            "\n");
+            "IHEVCE ERROR: Frame rate (%d / %d) is out of range [%.1f - %.1f]\n",
+            ps_static_cfg_prms->s_src_prms.i4_frm_rate_num,
+            ps_static_cfg_prms->s_src_prms.i4_frm_rate_denom,
+            MIN_FRAME_RATE, MAX_FRAME_RATE);
         return IHEVCE_SETUNSUPPORTEDINPUT(error_code);
     }
 
