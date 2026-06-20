4013d46862ebf53f2ffc1a68f88d238da158e480
Harish Mahendrakar <hmahendrakar@google.com>
2026-06-20 10:39:42 -0700

    CMakeLists.txt: Fix warnings seen in cmake

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -124,7 +124,7 @@ function(libhevc_add_gtest)
   include(FetchContent)
   FetchContent_Declare(
     googletest
-    URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+    URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
   )
   # For Windows: Prevent overriding the parent project's compiler/linker settings
   set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
